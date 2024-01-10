b0eb68d5b64735f332bbfdec14d6c597ac276e8f
Mathilde Besse <mathildebesse07@gmail.com>
2024-01-10 18:14:03 +0100

    ajout des boutons, ajouts des clicks sur les boutons, et ajout des filtres dans la console

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,18 +1,33 @@
 let workData = [];
+let categoriesData = [];
+
+const objects = document.querySelector(".objects");
+const all = document.querySelector(".all");
+const apartments = document.querySelector(".apartments");
+const hotels = document.querySelector(".hotels");
 const gallery = document.querySelector(".gallery");
-let i = 0;
 
 // variable pour récupérer données API
+//les projets
 const fetchWork = async () => {
   await fetch("http://localhost:5678/api/works")
     .then((res) => res.json())
     .then((data) => (workData = data));
-  console.log(workData);
+  //console.log(workData);
 };
-//afficher le display avec une async pour avoir le temps de récupérer les données de l'api
+//Les catégories
+const fetchCategory = async () => {
+  await fetch("http://localhost:5678/api/categories")
+    .then((res) => res.json())
+    .then((category) => (categoriesData = category));
+  //console.log(categoriesData);
+};
+fetchCategory();
+
+//afficher le display avec une async pour avoir le temps de récupérer
+//les données de l'API
 const workDisplay = async () => {
   await fetchWork();
-
   gallery.innerHTML = workData
     .map(
       (work) =>
@@ -27,3 +42,54 @@ const workDisplay = async () => {
 };
 
 workDisplay();
+
+//filtrer les projets
+const filterObjects = async () => {
+  await fetchWork();
+  const objectsFilter = workData.filter((work) => {
+    return work.category.name === "Objets";
+  });
+  console.log(objectsFilter);
+};
+
+const filterApartments = async () => {
+  await fetchWork();
+  const apartmentsFilter = workData.filter((work) => {
+    return work.category.name === "Appartements";
+  });
+  console.log(apartmentsFilter);
+};
+
+const filterHotels = async () => {
+  await fetchWork();
+  const hotelsFilter = workData.filter((work) => {
+    return work.category.name === "Hotels & restaurants";
+  });
+  console.log(hotelsFilter);
+};
+
+const filterAll = async () => {
+  await fetchWork();
+  const allFilter = workData;
+  console.log(allFilter);
+};
+
+objects.addEventListener("click", () => {
+  filterObjects();
+  //gallery.innerHTML = "";
+});
+
+apartments.addEventListener("click", () => {
+  filterApartments();
+});
+
+hotels.addEventListener("click", () => {
+  filterHotels();
+});
+
+all.addEventListener("click", () => {
+  filterAll();
+});
+
+//filterObjects();
+//console.log(arrayNumber.filter((number) => number > 10)
